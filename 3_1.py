23aee7e1526b7d370394c389fe59d25aef4708e4
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-06-30 18:27:58 +0300

    initial commit

--- 3_1.py
+++ 3_1.py
@@ -8,8 +8,6 @@ print(user_sentence_new)
 
 # Способ второй через .split и .join
 
-
-user_sentence: str = input('Введите предложение: ')
 user_sentence_split: str = user_sentence.split(' ')
 user_sentence_join: str = '-'.join(user_sentence_split)
 print(user_sentence_join)
