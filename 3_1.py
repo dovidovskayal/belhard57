3f742e5c42e200e4c39d05f5375569ea43e0e2c1
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-06-30 17:50:41 +0300

    initial commit

---
+++ 3_1.py
@@ -0,0 +1,15 @@
+# Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
+# Способ первый через .replace
+
+user_sentence: str = input('Введите предложение: ')
+user_sentence_new: str = user_sentence.replace(' ', '-')
+print(user_sentence_new)
+
+
+# Способ второй через .split и .join
+
+
+user_sentence: str = input('Введите предложение: ')
+user_sentence_split: str = user_sentence.split(' ')
+user_sentence_join: str = '-'.join(user_sentence_split)
+print(user_sentence_join)
