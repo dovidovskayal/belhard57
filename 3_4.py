d60207ece6afe3069d7d00eb6df3b43e3f4a839e
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-06-30 18:52:08 +0300

    initial commit

---
+++ 3_4.py
@@ -0,0 +1,11 @@
+# Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
+
+first_number: int = int(input('Введите первое число: '))
+second_number: int = int(input('Введите второе число: '))
+third_number: int = int(input('Введите третье число: '))
+first_number_res: bool = bool(first_number > 0)
+second_number_res: bool = bool(second_number > 0)
+third_number_res: bool = bool(third_number > 0)
+positive_result: int = first_number_res + second_number_res + third_number_res
+print('Положительных чисел: ',positive_result)
+print('Отрицательных чисел: ', 3 - positive_result)
