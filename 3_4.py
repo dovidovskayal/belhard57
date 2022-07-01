fced69170c3477cfa4e6b85ee9e5908e034fdb16
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-01 15:37:06 +0300

    initial commit

--- 3_4.py
+++ 3_4.py
@@ -7,5 +7,5 @@ first_number_res: bool = first_number > 0
 second_number_res: bool = second_number > 0
 third_number_res: bool = third_number > 0
 positive_result: int = first_number_res + second_number_res + third_number_res
-print('Положительных чисел: ',positive_result)
+print('Положительных чисел: ', positive_result)
 print('Отрицательных чисел: ', 3 - positive_result)
