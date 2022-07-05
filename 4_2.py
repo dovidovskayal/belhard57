d4332118a08e48fa396ca8cb596756f22b4c9b5f
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-05 15:54:15 +0300

    initial commit

--- 4_2.py
+++ 4_2.py
@@ -5,5 +5,5 @@ letters_dict ={}
 text: str = input('Введите текст: ')
 text_list: list = list(text)
 n: int = len(text_list)
-letters_dict = {text_list[i]: text_list.count(text_list[i]) for i in range(n)}
+letters_dict: dict = {text_list[i]: text_list.count(text_list[i]) for i in range(n)}
 print(letters_dict)
--- 4_3.py
+++ 4_3.py
@@ -3,14 +3,6 @@
 
 final_dict: dict = {}
 n = int(input('Введите число n: '))
-i = 0
-while i<=n-1:
-    name_email_dict: dict = {}
-    name = input('Введите имя: ')
-    name_email_dict['name'] = name
-    email = input('Введите свой email: ')
-    name_email_dict['email'] = email
-    final_dict[i] = name_email_dict
-    i+=1
+final_dict = {n: {"name": input('Введите имя: '), "email": input('Введите email: ')} for n in range(n)}
 print(final_dict)
 
