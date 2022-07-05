3a89adf12f10c4e8b85ed80a820c13c74101101d
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-05 14:49:02 +0300

    initial commit

--- 4_2.py
+++ 4_2.py
@@ -4,9 +4,6 @@
 letters_dict ={}
 text: str = input('Введите текст: ')
 text_list: list = list(text)
-text_list_len: int = len(text_list)
-i =0
-while i<=text_list_len-1:
-    letters_dict[text_list[i]] = text_list.count(text_list[i])
-    i+=1
+n: int = len(text_list)
+letters_dict = {text_list[i]: text_list.count(text_list[i]) for i in range(n)}
 print(letters_dict)
