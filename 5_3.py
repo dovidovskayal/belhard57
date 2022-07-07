9875f34b4466f900aec89acac02e09221c73536a
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-07 15:22:25 +0300

    initial commit

--- 5_3.py
+++ 5_3.py
@@ -1,9 +1,9 @@
 # Вывести четные числа от 2 до N по 5 в строку
 
 N: int = int(input())
-some_lst: list[int] = [i for i in range(2, N+1) if i%2==0]
+some_lst: list[int] = [i for i in range(2, N + 1) if i % 2 == 0]
 for i in some_lst:
-    a:int = some_lst.index(i)+1
+    a: int = some_lst.index(i) + 1
     if not a % 5 == 0 or a == 0:
         print(i, end=' ')
     else:
