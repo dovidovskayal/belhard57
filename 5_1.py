442171711dfc0f2f9ac04e3f0a2c2c4122fae5c7
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-07 13:35:54 +0300

    initial commit

---
+++ 5_1.py
@@ -0,0 +1,10 @@
+#Вывести первые N цисел кратные M и больше K
+
+N = int(input('Введите число N: '))
+M = int(input('Введите кратное число M: '))
+K = int(input('Введите число K: '))
+a = 1
+for i in range(10000):
+    if i % M == 0 and i > K and a <= N:
+            a += 1
+            print(i)
