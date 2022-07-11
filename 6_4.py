2b94c3ed3d42a4f42ea5d7b9aa215347e8cd7e57
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-11 15:01:33 +0300

    initial commit

--- 6_4.py
+++ 6_4.py
@@ -1,9 +1,7 @@
 #Дан список содержащий в себе различные типы данных,
 # отфильтровать таким образом, чтобы остались только строки, использование дополнительного списка незаконно
-def onlystr(any_lst):
-    any_lst = list(filter(lambda x: isinstance(x, str), any_lst))
-    return any_lst
 
-lst: list = ['hello', 3, (93, 5), 'python', [3, 'hell']]
-a = onlystr(lst)
-print(a)
+
+any_lst: list = ['hello', '3', (93, 5), 'python', [3, 'hell']]
+any_lst = list(filter(lambda x: isinstance(x, str), any_lst))
+print(any_lst)
