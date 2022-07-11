ac3a82bf06b28a3a4d7073027d3b1366ffc37fa9
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-11 23:56:15 +0300

    initial commit

--- 6_3.py
+++ 6_3.py
@@ -2,15 +2,16 @@
 # необходимо сместить список на указанное число, пример: [1,2,3,4,5,6,7] N=3 ответ: [5,6,7,1,2,3,4]
 
 def foo(N, numbers_list):
-    new_lst =[]
+    new_lst: list[int] = []
     for i in range(len(numbers_list)):
-        if i < len(numbers_list) - N :
-            new_lst.insert(i+N,numbers_list[i] )
+        if i < len(numbers_list) - N:
+            new_lst.insert(i + N, numbers_list[i])
         else:
-            new_lst.insert(N-len(numbers_list)+i,numbers_list[i])
+            new_lst.insert(N - len(numbers_list) + i, numbers_list[i])
     return new_lst
 
+
 N = int(input('Введите N: '))
-numbers_list = [1, 2, 3, 4, 5, 6, 7]
-result_list = foo(N, numbers_list)
+numbers_list: list[int] = [1, 2, 3, 4, 5, 6, 7]
+result_list: list[int] = foo(N, numbers_list)
 print(result_list)
