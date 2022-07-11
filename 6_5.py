fbeedfdd9277301ad99a6981658ba9a3d99988e6
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-11 20:23:16 +0300

    initial commit

--- 6_5.py
+++ 6_5.py
@@ -1,2 +1,12 @@
 # Дан список чисел, необходимо его развернуть без использования метода revese и функции reversed,
 # а так же дополнительного списка и среза
+def foo(lst):
+    for i in range(len(lst)//2):
+        temp = lst[i]
+        lst[i] = lst[-i-1]
+        lst[-i-1] = temp
+    return lst
+numbers_lst = [1,2,3,4,5,6,7,8,9]
+a = foo(numbers_lst)
+#numbers_lst = list(map(foo(numbers_lst), numbers_lst))
+print(a)
