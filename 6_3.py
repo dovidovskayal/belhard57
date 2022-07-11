9de630b3779d1bbd7948f8015640bfb35dd3758f
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-11 15:02:30 +0300

    initial commit

--- 6_3.py
+++ 6_3.py
@@ -5,13 +5,12 @@ def smeshenie(N, numbers_list):
     new_lst =[]
     for i in range(len(numbers_list)):
         if i < len(numbers_list) - N :
-            print(i)
             new_lst.insert(i+N,numbers_list[i] )
         else:
             new_lst.insert(N-len(numbers_list)+i,numbers_list[i])
     return new_lst
 
-N = int(input())
+N = int(input('Введите N: '))
 numbers_list = [1, 2, 3, 4, 5, 6, 7]
 result_list = smeshenie(N, numbers_list)
 print(result_list)
