5092fb8eb6826f0e08a3867cd0f5a955ed20913b
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-14 18:28:03 +0300

    initial commit

--- 6_7.py
+++ 6_7.py
@@ -1,12 +1,18 @@
 # Дан список чисел, необходимо для каждого элемента посчитать сумму его соседей,
 # для крайних чисел одним из соседей является число с противоположной стороны списка
 def foo(lst):
+    #for i in range(len(lst)):
+        #if i < len(lst) - 1:
+           # print('Для элемента', i, ':', lst[i-1], '+', lst[i+1], '=', lst[i-1]+lst[i+1])
+       # else:
+            #print('Для элемента', i, ':', lst[i-1], '+', lst[-i+1], '=', lst[i-1]+lst[-i+1])
+    neighbour = []
     for i in range(len(lst)):
-        if i < len(lst) - 1:
-            print('Для элемента', i, ':', lst[i-1], '+', lst[i+1], '=', lst[i-1]+lst[i+1])
+        if i == len(lst)-1:
+            neighbour.append(lst[i-1] + lst[0])
         else:
-            print('Для элемента', i, ':', lst[i-1], '+', lst[-i+1], '=', lst[i-1]+lst[-i+1])
-
+            neighbour.append(lst[i-1] + lst[i+1])
+    return neighbour
 
 some_list: list[int] = [12,13,11,22,24]
-foo(some_list)
+foo(foo(some_list))
