9f7133cc85f5897f99d9af5900aaab07cee4889c
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-14 18:21:24 +0300

    initial commit

--- 6_6.py
+++ 6_6.py
@@ -3,4 +3,12 @@ list_numbers: list[int] = [1,3,2,6,64,3,14,8,65,9,56,11]
 flist_numbers: list[int] = list(filter(lambda x: x%2 == 0, list_numbers))
 llist_numbers: list[int] = list(filter(lambda x: x%2 != 0, list_numbers))
 flist_numbers.extend(llist_numbers)
-print(flist_numbers)
+def sorted(numbers):
+    i = 0
+    while i<len(numbers):
+        if not numbers[i]%2:
+            numbers.insert(0,numbers.pop(i))
+        else:
+            i+=1
+    return numbers
+print(sorted(list_numbers))
