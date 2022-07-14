b84f23fbb083e8888726510b303f7e3c1d608dcf
LianaDovidovskaya <lianadovidovskaya@yandex.bygit config --global user.email lianadovidovskaya@yandex.b>
2022-07-14 18:08:21 +0300

    initial commit

--- 6_2.py
+++ 6_2.py
@@ -1,7 +1,7 @@
 # Код Морзе для представления цифр и букв использует тире и точки.
 # Напишите функцию для кодирования текстового сообщения в соответствии с кодом Морзе. (словари в помощь)
 def morze(message):
-    message_list = list(message)
+    #message_list = list(message)
     dict_lett: dict = {
         'a': '.-',
         'b': '-...',
@@ -41,11 +41,15 @@ def morze(message):
         '8': '---..',
         '9': '----.'
     }
-    for i in range(len(message_list)):
-        message_list[i] = dict_lett.get(message_list[i], 'значения нет')
-    message = ''.join(message_list)
-    return message
-
+    #for i in range(len(message_list)):
+     #  message_list[i] = dict_lett.get(message_list[i], 'значения нет')
+    #message = ''.join(message_list)
+    #return message
+    morze_message = ''
+    for letter in message.lower():
+        if letter in dict_lett:
+            morze_message +=dict_lett[letter]
+    return (morze_message)
 
 message: str = input('Введите сообщение: ')
 new_message: str = morze(message.lower())
